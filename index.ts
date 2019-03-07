30e68c07b01e88ded9e169d9b7af9247317ba960
wanglihui <lihui.wang@jingli365.com>
2019-03-07 18:16:29 +0800

    package.json

--- index.ts
+++ index.ts
@@ -13,15 +13,21 @@ export class RepeateActionFilter {
         return new Promise(async (resolve, reject) => { 
             try {
                 //已经存在
-                if (this.queues.indexOf(action) >= 0) {
+                let idx = this.queues.indexOf(action)
+                if (idx >= 0) {
                     return this.listeners[action].push((ret: any) => {
                         resolve(ret);
                     });
                 }
                 this.queues.push(action);
                 this.listeners[action] = []
+
                 let ret = await handle();
                 let fns = this.listeners[action];
+                //设置为空
+                delete this.listeners[action];
+                this.queues.splice(idx, 1);
+                //处理注册的函数
                 let ps = fns.map(async fn => {
                     return fn(ret);
                 });
